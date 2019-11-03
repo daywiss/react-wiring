13e6ee3de8db8ec661ce3b294eea5e06f106f6ac
Jacob Dent <jdent@tacyarg.com>
2019-11-03 13:58:15 -0400

    map parent props

--- connect.js
+++ connect.js
@@ -1,15 +1,18 @@
 import assert from './assert'
-export default (React,Context,dispatch) => {
-  assert(React,'requires react library')
-  assert(Context,'requires react context')
-  return (Child,map=x=>x) => {
-    return (props={})=>{
-      return React.createElement(
-        Context.Consumer,
-        null,
-        state=>React.createElement(Child,{ dispatch, ...map(state), ...props, })
+export default (React, Context, dispatch) => {
+  assert(React, 'requires react library')
+  assert(Context, 'requires react context')
+  return (Child, map = x => x) => {
+    return (props = {}) => {
+      return React.createElement(Context.Consumer, null, state =>
+        React.createElement(Child, {
+          dispatch,
+          ...map({
+            ...state,
+            ...props,
+          }),
+        })
       )
     }
   }
 }
-
