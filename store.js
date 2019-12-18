bac3582dc20841319ddbade86342574006944a5d
david <daywiss@gmail.com>
2019-12-18 02:03:32 +0000

    working and api finalized

---
+++ store.js
@@ -0,0 +1,80 @@
+import get from 'lodash.get'
+import isArray from 'lodash.isarray'
+import isFunction from 'lodash.isfunction'
+import isString from 'lodash.isstring'
+import assert from './assert'
+
+export default (reducers,state={})=>{
+
+  const listeners = new Map()
+
+  function dispatch(action,...args){
+    const reducer = get(reducers,action)
+    assert(isFunction(reducer),`No reducer for action ${action}`)
+    return set( reducer(state, ...args) )
+  }
+
+  const curry = action => (...args) => dispatch(action,...args)
+
+  function set(next){
+    for (let [cb, isEqual] of listeners.entries()){
+      if(!isEqual(state,next)) cb(next)
+    }
+    state = next
+  }
+
+  const wrapPathArray = (paths=[]) => (prev,next)=>{
+    //if an empty array is passed trigger every state update
+    if(paths.length == 0) return false
+    return  paths.every(path=>{
+      return get(prev,path) == get(next,path)
+    })
+  }
+
+  const wrapPathString = (path) => (prev,next)=>{
+    //if empty string passed trigger every update
+    if(path.length == 0) return false
+    return get(prev,path) == get(next,path)
+  }
+
+  function on(isEqual,cb){
+    assert(isFunction(cb),'requires callback function')
+    
+    cb(state)
+
+    if(isEqual){
+      if(isString(isEqual)){
+        listeners.set(cb,wrapPathString(isEqual))
+      }
+      else if(isArray(isEqual)){
+        listeners.set(cb,wrapPathArray(isEqual))
+      }
+      else if(isFunction(isEqual)){
+        listeners.set(cb,isEqual)
+      }else{
+        console.log({isEqual})
+        throw new Error('isEqual must be string, array of strings, array of arrays, or a function')
+      }
+    }
+
+    return ()=>off(cb)
+  }
+
+  function off(cb){
+    return listeners.delete(cb)
+  }
+
+  function getState(){
+    return state
+  }
+
+  return {
+    dispatch,
+    curry,
+    on,
+    off,
+    get:getState,
+    set,
+  }
+}
+
--- wiring.js
+++ wiring.js
@@ -1,50 +1,22 @@
 import Provider from './provider'
 import assert from './assert'
+import Store from './store'
 
-export default (React,reducers={},state={}) => {
-  assert(React,'requires react library')
+export default (React,reducers,state) => {
+  assert(React,'requires react >=16.8')
+  const {useState,useEffect,useRef} = React
+  assert(useState,'requires react useState')
+  assert(useEffect,'requires react useEffect')
 
-  const contexts = {
-    dispatch:React.createContext(),
-    state:React.createContext(),
-    constants:React.createContext(),
-  }
-
-  const provider = Provider(React,contexts,reducers,state)
-
-  const Use = (name,Context) => () =>{
-    const context = React.useContext(Context)
-    if (context === undefined) {
-      throw new Error(`use${name} must be used within a ReactWiring Provider`)
-    }
-    return context
-  }
-
-  const useState = Use('State',contexts.state)
-  const useDispatch = Use('Dispatch',contexts.dispatch)
-  const useConstants = Use('Constants',contexts.constants)
-
-  const useWiring = ()=>{
-    return [
-      useState(),
-      useDispatch(),
-      useConstants(),
-    ]
-  }
+  const store = Store(reducers,state)
 
   return {
-    WiringProvider:provider,
-    useWiring,
-    useState,
-    useDispatch,
-    useConstants,
-    connect(Child,isEqual,map=x=>x){
-      const memo = React.memo(Child,isEqual)
-      return props=>{
-        const [state,dispatch,constants] = useWiring()
-        return React.createElement(memo,{dispatch,...constants,...map(state),...props})
-      }
-    }
+    useWiring(isEqual,map=x=>x){
+      const [state,setState] = useState(map(store.get()))
+      useEffect(x=>store.on(isEqual,state=>setState(map(state))),[])
+      return [state,store.dispatch,store.curry]
+    },
+    store
   }
 
 }
